e3dac334badcc6c7879d227363280d81529e4b77
Roopesh Patel <Roops.pesh@gmail.com>
2021-05-09 20:57:39 +0100

    refactor group name edit

--- src/components/editGroupNameMenuItem.tsx
+++ src/components/editGroupNameMenuItem.tsx
@@ -1,8 +1,7 @@
-import { Popover, MenuItem } from "@material-ui/core"
 import React, { useState } from "react"
-import useWindowResize from "../utils/useWindowResize"
 import TaskGroupChangeNameForm from "./forms/taskGroupChangeNameForm"
 import { TaskGroupType } from "../redux/projectSlice/projectSlice"
+import MenuItemPopover from "./menuItemPopover"
 
 interface Props {
   group: TaskGroupType
@@ -11,8 +10,6 @@ interface Props {
 
 const EditGroupNameMenuItem: React.FC<Props> = ({ group, handleClose }) => {
   const [isPopoverOpen, setPopoverOpen] = useState(false)
-  const [width, height] = useWindowResize()
-  const id = isPopoverOpen ? "edit-group-name-popover" : undefined
 
   const closePopover = () => {
     setPopoverOpen(false)
@@ -23,26 +20,14 @@ const EditGroupNameMenuItem: React.FC<Props> = ({ group, handleClose }) => {
     handleClose()
   }
   return (
-    <>
-      <MenuItem dense onClick={handleClick}>
-        Edit Group Name
-      </MenuItem>
-      <Popover
-        anchorReference="anchorPosition"
-        anchorPosition={{
-          top: (typeof window !== "undefined" && height && height / 2) || 0,
-          left: (typeof window !== "undefined" && width && width / 2) || 0,
-        }}
-        transformOrigin={{
-          vertical: "center",
-          horizontal: "center",
-        }}
-        id={id}
-        open={isPopoverOpen}
-      >
-        <TaskGroupChangeNameForm group={group} closePopover={closePopover} />
-      </Popover>
-    </>
+    <MenuItemPopover
+      title="Edit Group Name"
+      popoverId="Edit-Group-Name-popover"
+      open={isPopoverOpen}
+      onClick={handleClick}
+    >
+      <TaskGroupChangeNameForm group={group} closePopover={closePopover} />
+    </MenuItemPopover>
   )
 }
 
