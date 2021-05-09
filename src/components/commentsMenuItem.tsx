eb499713c97931c9e6848b10acdd370cae6bc3ea
Roopesh Patel <Roops.pesh@gmail.com>
2021-05-09 20:45:58 +0100

    refactor comments menu item

--- src/components/commentsMenuItem.tsx
+++ src/components/commentsMenuItem.tsx
@@ -1,10 +1,9 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/react"
-import { Popover, MenuItem } from "@material-ui/core"
 import React, { useState } from "react"
 import { TaskType, ChildTaskType } from "../redux/projectSlice/projectSlice"
-import useWindowResize from "../utils/useWindowResize"
 import CommentsPopover from "./commentsPopover"
+import MenuItemPopover from "./menuItemPopover"
 
 type Props = (task | childTask) & { handleClose: () => void }
 
@@ -19,8 +18,6 @@ interface childTask {
 
 const CommentsMenuItem: React.FC<Props> = props => {
   const [isPopoverOpen, setPopoverOpen] = useState(false)
-  const [width, height] = useWindowResize()
-  const id = isPopoverOpen ? "add-task-popover" : undefined
 
   const closePopover = () => {
     setPopoverOpen(false)
@@ -32,38 +29,23 @@ const CommentsMenuItem: React.FC<Props> = props => {
   }
 
   return (
-    <>
-      <MenuItem dense onClick={handleClick}>
-        Comments
-      </MenuItem>
-      <Popover
-        css={{ "& .MuiPopover-paper": { minWidth: "70%" } }}
-        anchorReference="anchorPosition"
-        anchorPosition={{
-          top: (typeof window !== "undefined" && height && height / 2) || 0,
-          left: (typeof window !== "undefined" && width && width / 2) || 0,
-        }}
-        transformOrigin={{
-          vertical: "center",
-          horizontal: "center",
-        }}
-        id={id}
-        open={isPopoverOpen}
-      >
-        <CommentsPopover
-          closePopover={closePopover}
-          comments={props.task.comments}
-          taskId={
-            props.type === "task" ? props.task.taskId : props.task.childTaskId
-          }
-          taskName={
-            props.type === "task"
-              ? props.task.taskName
-              : props.task.childTaskName
-          }
-        />
-      </Popover>
-    </>
+    <MenuItemPopover
+      title="Comments"
+      popoverId="comments-popover"
+      open={isPopoverOpen}
+      onClick={handleClick}
+    >
+      <CommentsPopover
+        closePopover={closePopover}
+        comments={props.task.comments}
+        taskId={
+          props.type === "task" ? props.task.taskId : props.task.childTaskId
+        }
+        taskName={
+          props.type === "task" ? props.task.taskName : props.task.childTaskName
+        }
+      />
+    </MenuItemPopover>
   )
 }
 
