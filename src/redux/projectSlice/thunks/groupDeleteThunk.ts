607e1b0e0fe47c1cf3256530d1b762b09c26b8ab
Roopesh Patel <Roops.pesh@gmail.com>
2021-05-12 21:09:23 +0100

    cleanup

--- src/redux/projectSlice/thunks/groupDeleteThunk.ts
+++ src/redux/projectSlice/thunks/groupDeleteThunk.ts
@@ -2,11 +2,6 @@ import { createAsyncThunk } from "@reduxjs/toolkit"
 import { RootState } from "../../store"
 import faunaClient from "../../../utils/faunaClient"
 
-type GroupDeleteReturnType = {
-  groupId: string
-  projectId: string
-}
-
 const deleteGroup = createAsyncThunk<
   { projectId: string; groupId: string },
   { groupId: string; projectId: string },
@@ -18,9 +13,7 @@ const deleteGroup = createAsyncThunk<
     if (secret) {
       const { client, q } = faunaClient(secret)
 
-      const data: GroupDeleteReturnType = await client.query(
-        q.Call("taskGroupDelete", [groupId, userId])
-      )
+      await client.query(q.Call("taskGroupDelete", [groupId, userId]))
 
       return { groupId, projectId }
     } else {
