3479e0227e4905fe6028e74caf08914ea01f3006
roopsBee <35971830+roopsBee@users.noreply.github.com>
2021-05-27 20:00:02 +0100

    animate settings page

--- src/components/clientRoutes/projectSettingsPage.tsx
+++ src/components/clientRoutes/projectSettingsPage.tsx
@@ -12,6 +12,7 @@ import React from "react"
 import { useAppSelector } from "../../redux/reduxHooks"
 import DeleteProjectListItem from "../settingsItems/deleteProjectListItem"
 import ChangeProjectNameListItem from "../settingsItems/changeProjectNameListItem"
+import { motion, Variants } from "framer-motion"
 
 interface Props {
   projectId: string
@@ -24,21 +25,34 @@ const ProjectSettingsPage: React.FC<Props> = ({ projectId }) => {
     )
   )
 
+  const variants: Variants = {
+    visible: { height: "initial" },
+    hidden: { height: 0 },
+  }
+
   return (
     <>
       <Container maxWidth="xs" disableGutters css={{ marginTop: 8 }}>
-        <Paper css={{ padding: "8px 0px" }}>
-          <Grid container item xs={12} justify="center">
-            <Typography variant="h4">
-              Settings: {project?.projectName}
-            </Typography>
-          </Grid>
-          <List>
-            <Divider />
-            <ChangeProjectNameListItem />
-            <DeleteProjectListItem projectId={projectId} />
-          </List>
-        </Paper>
+        <motion.div variants={variants} initial="hidden" animate="visible">
+          <Paper css={{ padding: "8px 0px" }}>
+            <Grid container item xs={12} justify="center">
+              <motion.div variants={variants} css={{ overflow: "hidden" }}>
+                <Typography variant="h4">
+                  Settings: {project?.projectName}
+                </Typography>
+              </motion.div>
+            </Grid>
+            <List>
+              <Divider />
+              <motion.div variants={variants} css={{ overflow: "hidden" }}>
+                <ChangeProjectNameListItem />
+              </motion.div>
+              <motion.div variants={variants} css={{ overflow: "hidden" }}>
+                <DeleteProjectListItem projectId={projectId} />
+              </motion.div>
+            </List>
+          </Paper>
+        </motion.div>
       </Container>
     </>
   )
